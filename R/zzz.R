cc7055a2ab74f929c07639f7b4aaf5fd7deee692
Tal Galili <tal.galili@gmail.com>
2020-02-22 17:53:34 +0200

    Update to 3.0.2: take ownership so to keep on CRAN.

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+# shell('git log --graph --stat --date=short --pretty=format:"%ad(%an) %s |%h" > ChangeLog', intern = TRUE)
+# system('git log --graph --stat --date=short --pretty=format:"%ad(%an) %s |%h" > ChangeLog', intern = TRUE)
+# file.copy("NEWS", "NEWS.md",overwrite = TRUE)
+# devtools::check_win_devel()
+# pkgdown::build_home()
+# pkgdown::build_site(run_dont_run = TRUE)
+# release()
