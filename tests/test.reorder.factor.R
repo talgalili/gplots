68f8a050e960f8005e3fa9031ca3cd76a9c4b30f
Tal Galili <tal.galili@gmail.com>
2020-09-12 10:50:31 +0300

    Fix a bunch of errors

--- tests/test.reorder.factor.R
+++ tests/test.reorder.factor.R
@@ -2,10 +2,8 @@
 
 m <- factor(c('a','b','c'))
 
-( m1 <- reorder(m, X=c(3, 2, 1)) )
+( m1 <- stats::reorder(m, X=c(3, 2, 1)) )
 
-library(gdata)
-
-( m2 <- reorder(m, X=c(3, 2, 1)) )
+( m2 <- gplots::reorder.factor(m, X=c(3, 2, 1)) )
 
 stopifnot(identical(m1,m2))
